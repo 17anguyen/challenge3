57f646d124b107a4cfcee1a873384e63c8c3306f
17anguyen <43556891+17anguyen@users.noreply.github.com>
2023-03-30 21:23:09 -0700

    finally got password to output corrected typos

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var characters = ['!', '@', '#', '$', '%']
-var all = [lowerCase, upperCase, nums, characters];
+var all = (lowerCase, upperCase, nums, characters)
 var availChars = []
 
 // TODO: ASK FOR HELP WITH LENGTH INPUT NOT SEEMING RANDOM
@@ -14,9 +14,82 @@ var availChars = []
 // TODO: restart for a new password
 // var restartGen= true;
 // while(restartGen) {}
-
 // pwd length 
+function generatePassword() {
+  var outputPwd = ""
 
+
+
+  // TODO: how to return a passsword?
+  var pwdLength =
+    prompt("Please enter the length of your password (between 8 and 128 characters):");
+  console.log(pwdLength)
+  if (pwdLength < 8 || pwdLength > 128) {
+    alert("Can you read? Select a number between 8 and 128")
+    return
+  } else {
+
+
+    // lowerCase
+    var userLowerCase = confirm("Do you want lower case letters?");
+    if (userLowerCase == true) {
+
+      alert("have you ever played geoguesser? bet you could beat me. GG EZ")
+  
+        availChars = availChars.concat(lowerCase)
+    } else {
+      alert("Fine. Have it your way.")
+    }
+
+    // uppercase
+    var userUpperCase = confirm("Do you want upper case letters?");
+    if (userUpperCase == true) {
+
+      alert("AAAHHH")
+      availChars = availChars.concat(upperCase)
+    
+    } else {
+      alert("WHY AM I YELLING?")
+    }
+
+    // numbers
+    var userNums = confirm("Do you want numbers?");
+    if (userNums == true) {
+
+      alert("What comes after 9?")
+      availChars = availChars.concat(nums)
+      
+    } else {
+      alert("It's okay I don't like math either")
+    }
+
+    // characters
+    var userCharacters = confirm("Do you want special characters?");
+    if (userCharacters == true) {
+
+      alert("Is 'special characters' PC?")
+      availChars = availChars.concat(characters)
+
+    } else {
+      alert("I want to cry I am so tired")
+    }
+    console.log(availChars)
+
+
+    // build password
+    var outputPwd = ""
+    for (let i = 0; i < pwdLength; i++) {
+      var randomInput = availChars [Math.floor(Math.random()*availChars.length)]
+      outputPwd += randomInput
+      
+    }
+    return outputPwd
+  }
+  
+}
+      // generate random number in between 0 and the length avail chars.length
+      // get random char by referncing availChars at the index of random num
+      // add random char to passwo1rd
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -28,83 +101,7 @@ function writePassword() {
 
 
 }
-var start = false;
-generateBtn.addEventListener("click", function () {
-  if (!start) {
-    generatePassword();
-    start = true;
-  }
-  generateBtn.addEventListener("click", writePassword);
-
-
-
-});
-
-function generatePassword() {
-  // TODO: how to return a passsword?
-  var pwdLength =
-    prompt("Please enter the length of your password (between 8 and 128 characters):");
-  if (pwdLength < 8 || pwdLength > 128) {
-    alert("Can you read? Select a number between 8 and 128")
-    generatePassword();
-  } else {
-    alert("testing if this works");
-
-    // lowerCase
-    var userLowerCase = confirm("Do you want lower case letters?");
-    if (userLowerCase == true) {
-      alert("have you ever played geoguesser? bet you could beat me. GG EZ")
-      availChars = availChars.concat(lowerCase)
-
-    } else {
-      alert("Fine. Have it your way.")
-    }
-
-
-    var userUpperCase = confirm("Do you want upper case letters?");
-    if (userUpperCase == true) {
-      alert("AAAHHH")
-      availChars = availChars.concat(upperCase)
-
-    } else {
-      alert("WHY AM I YELLING")
-    }
-
-
-    var userNums = confirm("Do you want numbers?");
-    if (userNums == true) {
-      alert("What comes after 9?")
-      availChars.concat(nums)
-
-    } else {
-      alert("It's okay I don't like math either")
-    }
-
-
-    var userCharacters = confirm("Do you want special characters?");
-    if (userCharacters == true) {
-      alert("Is 'special characters' PC?")
-      availChars.concat(characters)
-
-    } else {
-      alert("I want to cry I am so tired")
-    }
-
-
-
-    // build password
-    for (let i = 0; i < pwdLength; i++) {
-      var randomInput = Math.floor(Math.random() * availChars.length);
-      password = availChars[randomInput]
-
-    } console.log(availChars);
-    console.log(password);
-  }
-
-  return password
-}
-
-
+generateBtn.addEventListener("click", writePassword);
 
 
 // psuedo code
@@ -124,4 +121,3 @@ function generatePassword() {
 
 
 // Add event listener to generate button
-
